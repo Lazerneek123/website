acbc52aeb9e25f1f2896e4c438a933150ef0d46f
Lazerneek123 <Lazerneek123@yandex.ru>
2024-06-16 23:47:15 +0300

    Update first page

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -3,12 +3,15 @@ import './css/Main.css';
 import candidateImage from './resourse/images/candidate.jpg';
 import photo from './resourse/images/photo.jpg';
 import duct from './resourse/images/duct.png';
+import arrowRightUpBlack from './resourse/images/fontisto_arrow-right-black.png';
+import personImage from './resourse/images/candidate.jpg';
+import arrowRightUp from './resourse/images/arrow-right-up.png';
 
 
 function Title() {
   return (
     <div class="container">
-      <div class="header1">
+      <div class="divheader1">
         <div class="divHeader">
           <div class="divHeaderTitle">
             <h1 class="header1">САЙТ <span class="highlight">СПРОТИВУ</span></h1>
@@ -148,20 +151,26 @@ function Candidates() {
     <section class="news">
       <div class="app">
         <div class="divRating">
-          <p class="rating">Рейтинг зашкварів</p>
-          <p class="descriptionNews">Ця категорія сайту присвячена висвітленню депутатів та чиновників, які зрадили довіру народу та чинили злочини проти України.</p>
+          <div class="latest-rating-full-header">
+            <div class="latest-rating-left">
+              <div class="rating">РЕЙТИНГ ЗАШКВАРІВ</div>
+              <div class="descriptionNews">Ця категорія сайту присвячена висвітленню депутатів та чиновників, які зрадили довіру народу та чинили злочини проти України.</div>
+            </div>
+            <a class="latest-rating-right" href='/rating'>
+              <div class="labelRating">Рейтинг зашкварів</div>
+              <img class="arrowRightUpBlack" src={arrowRightUpBlack} />
+            </a>
+          </div>
         </div>
-        <div class="buttonRating">РЕЙТИНГ ЗАШКВАРІВ</div>
         <RepresentativeList />
         <div style={{ height: `4px` }}>
           <div class="progress-bar">
             <div class="progress" style={{
-              /*width: `${(currentPage / totalPages) * 100}%`*/
               width: `${widthLine}%`,
-                  height: '4px',
-                  backgroundColor: '#DE442C',
-                  position: 'absolute',
-                  left: `${(currentPage - 1) * widthLine}%`
+              height: '4px',
+              backgroundColor: '#DE442C',
+              position: 'absolute',
+              left: `${(currentPage - 1) * widthLine}%`
             }}></div>
           </div>
         </div>
@@ -178,42 +187,113 @@ function Candidates() {
 function Investigation() {
   return (
     <div class="appInvestigation">
-      <div class="headerInvestigation">
-        <h1>ПАМ'ЯТАЙ ПРО УСІ ЗАШКВАРИ ДЕПУТАТІВ ТА ЧИНОВНИКІВ</h1>
+      <div class="divRating">
+        <div class="latest-rating-full-header">
+          <div class="latest-investigation-left">
+            <div class="headerInvestigation">ПАМ'ЯТАЙ ПРО УСІ ЗАШКВАРИ ДЕПУТАТІВ ТА ЧИНОВНИКІВ</div>
+            <div class="subheaderInvestigation">Наша мета - інформувати громадськість про зашквари депутатів та чиновників, засудити їхню поведінку та сприяти їхньому покаранню.</div>
+          </div>
+
+        </div>
       </div>
-      <div class="subheaderInvestigation">
-        <p>Наша мета - інформувати громадськість про зашквари депутатів та чиновників, засудити їхню поведінку та сприяти їхньому покаранню.</p>
-      </div>
+
       <div class="sectionInvestigation">
-        <h2> ЗАШКВАРИ </h2>
-        <h2> ОСОБИ </h2>
+        <a class="investigation-header" href="">
+          ЗАШКВАРИ
+          <img class="arrowRightUp" src={arrowRightUp} />
+        </a>
+        <hr class="dividerInvestigation" />
+        <a class="investigation-header" href="">
+          ОСОБИ
+          <img class="arrowRightUp" src={arrowRightUp} />
+        </a>
       </div>
     </div>
   );
 }
 
-function News() {
+function LatestInvestigations() {
   return (
-    <section class="news">
-      <h2>Новини</h2>
-      <ul>
-        <li>
-          <img src="logo192.png" alt="Новина 1" />
-          <h3>Протест проти нового закону</h3>
-          <p>Сьогодні відбулася мирна демонстрація проти нового закону про обмеження прав людини.</p>
-        </li>
-        <li>
-          <img src="logo192.png" alt="Новина 2" />
-          <h3>Кампанія проти корупції</h3>
-          <p>Ми запустили нову кампанію проти корупції в Україні.</p>
-        </li>
-        <li>
-          <img src="logo192.png" alt="Новина 3" />
-          <h3>Допомога постраждалим від репресій</h3>
-          <p>Ми надаємо допомогу постраждалим від репресій та насильства.</p>
-        </li>
-      </ul>
-    </section>
+    <div class="latest-investigations-main">
+      <div class="latest-rating-full-header">
+        <div class="latest-rating-left">
+          <div class="rating">ОСТАННІ ЗАШКВАРИ</div>
+          <div class="descriptionNews">Оновлюваний перелік епізодів корупції, хабарництва, зловживання владою, некомпетентності та інших неприйнятних вчинків представників влади.</div>
+        </div>
+        <a class="latest-rating-right">
+          <div class="labelRating">Всі Зашквари</div>
+          <img class="arrowRightUpBlack" src={arrowRightUpBlack} />
+        </a>
+      </div>
+      <div class="latest-investigation-flow">
+        <div class="investigation-card">
+          <div class="investigation-card-content">
+            <div class="investigation-card-header">
+              <img class="person-icon" src={personImage} />
+              <div class="investigation-card-text">Maксим Петренко</div>
+              <div class="investigation-card-text">+5</div>
+            </div>
+            <span class="investigation-card-date ">22 БЕР 2024</span>
+            <div class="investigation-card-label">Незаконне збагачення політиків - це злочин проти народу: час покласти край!</div>
+          </div>
+        </div>
+        <div class="investigation-card">
+          <div class="investigation-card-content">
+            <div class="investigation-card-header">
+              <img class="person-icon" src={personImage} />
+              <div class="investigation-card-text">Maксим Петренко</div>
+              <div class="investigation-card-text">+5</div>
+            </div>
+            <span class="investigation-card-date ">22 БЕР 2024</span>
+            <div class="investigation-card-label">Незаконне збагачення політиків - це злочин проти народу: час покласти край!</div>
+          </div>
+        </div>
+        <div class="investigation-card">
+          <div class="investigation-card-content">
+            <div class="investigation-card-header">
+              <img class="person-icon" src={personImage} />
+              <div class="investigation-card-text">Maксим Петренко</div>
+              <div class="investigation-card-text">+5</div>
+            </div>
+            <span class="investigation-card-date ">22 БЕР 2024</span>
+            <div class="investigation-card-label">Незаконне збагачення політиків - це злочин проти народу: час покласти край!</div>
+          </div>
+        </div>
+        <div class="investigation-card">
+          <div class="investigation-card-content">
+            <div class="investigation-card-header">
+              <img class="person-icon" src={personImage} />
+              <div class="investigation-card-text">Maксим Петренко</div>
+              <div class="investigation-card-text">+5</div>
+            </div>
+            <span class="investigation-card-date ">22 БЕР 2024</span>
+            <div class="investigation-card-label">Незаконне збагачення політиків - це злочин проти народу: час покласти край!</div>
+          </div>
+        </div>
+        <div class="investigation-card">
+          <div class="investigation-card-content">
+            <div class="investigation-card-header">
+              <img class="person-icon" src={personImage} />
+              <div class="investigation-card-text">Maксим Петренко</div>
+              <div class="investigation-card-text">+5</div>
+            </div>
+            <span class="investigation-card-date ">22 БЕР 2024</span>
+            <div class="investigation-card-label">Незаконне збагачення політиків - це злочин проти народу: час покласти край!</div>
+          </div>
+        </div>
+        <div class="investigation-card">
+          <div class="investigation-card-content">
+            <div class="investigation-card-header">
+              <img class="person-icon" src={personImage} />
+              <div class="investigation-card-text">Maксим Петренко</div>
+              <div class="investigation-card-text">+5</div>
+            </div>
+            <span class="investigation-card-date ">22 БЕР 2024</span>
+            <div class="investigation-card-label">Незаконне збагачення політиків - це злочин проти народу: час покласти край!</div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
@@ -223,7 +303,7 @@ function Main() {
       <Title />
       <Candidates />
       <Investigation />
-      <News />
+      <LatestInvestigations />
     </div>
   );
 }
